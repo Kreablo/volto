10b04ab77cb67b3a60dcbd19818bf799135083be
Andreas Jonsson <andreas.jonsson@kreablo.se>
2026-03-16 13:39:54 +0100

    Search result cache

---
+++ packages/volto/src/actions/search/search.ts
@@ -0,0 +1,111 @@
+/**
+ * Search actions.
+ * @module actions/search/search
+ */
+
+import compact from 'lodash/compact';
+import concat from 'lodash/concat';
+import isArray from 'lodash/isArray';
+import join from 'lodash/join';
+import map from 'lodash/map';
+import pickBy from 'lodash/pickBy';
+import toPairs from 'lodash/toPairs';
+import { LRUCache } from 'lru-cache';
+import { Action } from 'redux';
+import {
+  RESET_SEARCH_CONTENT,
+  SEARCH_CONTENT,
+} from '@plone/volto/constants/ActionTypes';
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
+import { flattenToAppURL } from '../../helpers';
+
+const _request_cache = new LRUCache({
+  max: 100,
+  ttl: 1000 * 60 * 5,
+});
+
+/**
+ * Search content function.
+ * @function searchContent
+ * @param {string} url Url to use as base.
+ * @param {Object} options Search options.
+ * @param {string} subrequest Key of the subrequest.
+ * @returns {Object} Search content action.
+ */
+export function searchContent(url: string, options: Record<string, any>, subrequest: string | null = null): ThunkAction<Promise<any>, any, any, Action> {
+  let queryArray: string[] = [];
+  options = pickBy(
+    options,
+    (value) => value !== undefined && value !== null && value !== '',
+  );
+  const arrayOptions = pickBy(options, (item) => isArray(item));
+
+  queryArray = concat(
+    queryArray,
+    options
+      ? join(
+        map(toPairs(pickBy(options, (item) => !isArray(item))), (item) => {
+          if (item[0] === 'SearchableText') {
+            // Adds the wildcard to the SearchableText param
+            item[1] = `${item[1]}*`;
+          }
+          return join(item, '=');
+        }),
+        '&',
+      )
+      : '',
+  );
+
+  queryArray = concat(
+    queryArray,
+    arrayOptions
+      ? join(
+        map(pickBy(arrayOptions), (item, key) =>
+          join(
+            item.map((value) => `${key}:list=${value}`),
+            '&',
+          ),
+        ),
+        '&',
+      )
+      : '',
+  );
+
+  const querystring = join(compact(queryArray), '&');
+
+  return async (dispatch, getState) => {
+    const key = querystring;
+    const value = _request_cache.get(key) as { items: any[], items_total: number, batching: any } | undefined;
+    if (value !== undefined) {
+      return dispatch({
+        type: `${SEARCH_CONTENT}_SUCCESS`,
+        subrequest,
+        result: value,
+      });
+    }
+    const result = await dispatch({
+      type: SEARCH_CONTENT,
+      subrequest,
+      request: {
+        op: 'get',
+        path: `${url}/@search${querystring ? `?${querystring}` : ''}`,
+      },
+    });
+    const s = getState();
+    _request_cache.set(key, result);
+    return s.search;
+  };
+}
+
+/**
+ * Reset search content function.
+ * @function resetSearchContent
+ * @param {string} subrequest Key of the subrequest.
+ * @returns {Object} Search content action.
+ */
+export function resetSearchContent(subrequest = null) {
+  return {
+    type: RESET_SEARCH_CONTENT,
+    subrequest,
+  };
+}
